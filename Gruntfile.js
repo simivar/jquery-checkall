aa00f4b86d59da7cbe36e500ab20bf5faa3a0820
simivar <simivar@gmail.com>
2016-03-26 15:10:27 +0100

    grunt clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	grunt.registerTask('test', ['jshint', 'clean', 'concat', 'uglify']);
 
